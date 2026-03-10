ec936ebecae22d7fa0435ff6915016b4317d85f4
Apixelian <pixe366@gmail.com>
2026-03-10 14:17:29 -0500

    Modelos de todas las tablas hechas

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    // PK Personalizada
+    protected $primaryKey = 'course_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'name',
+        'course_key',
+        'title',
+        'cover',
+        'content',
+        'group_id',
+        'kit_id',
+    ];
+
+    public function kit()
+    {
+        return $this->belongsTo(Kit::class, 'kit_id');
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class, 'group_id');
+    }
 }
